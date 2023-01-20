c8aa497f60c8f0e2b3d57565916d9a90304f8ad1
28Pollux28 <lemaire_valentin28@yahoo.fr>
2023-01-20 10:36:25 +0100

    init commit

---
+++ models.py
@@ -0,0 +1,17 @@
+import multiprocessing
+
+
+class Home(multiprocessing.Process):
+    pass
+
+
+class Market(multiprocessing.Process):
+    pass
+
+
+class Weather(multiprocessing.Process):
+    pass
+
+
+class External(multiprocessing.Process):
+    pass
